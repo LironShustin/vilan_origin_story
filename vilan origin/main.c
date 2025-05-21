1f04c80dd1455572dbd509ef63542c0196a3e925
lironshustin1996 <lironshustin1996@gmail.com>
2025-05-21 15:47:14 +0300

    I added all the questions for all the genres

--- vilan origin/main.c
+++ vilan origin/main.c
@@ -1,3 +1,20 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//#include <stdbool.h>
+//#include "fileSaving.h"
+//#include "questions.h"
+//#include "editingFiles.h"
+//
+//int main() { 
+//	char answers_file[260];
+//	bool realistic = true;
+//	create_answers_file(&answers_file);
+//	is_realistic(&realistic, &answers_file);
+//	genre(realistic, &answers_file);
+//	how_long_you_met(&answers_file);
+//	first_meeting_place(&answers_file);
+//}
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,12 +23,29 @@
 #include "questions.h"
 #include "editingFiles.h"
 
-int main() { 
-	char answers_file[260];
-	bool realistic = true;
-	create_answers_file(&answers_file);
-	is_realistic(&realistic, &answers_file);
-	genre(realistic, &answers_file);
-	how_long_you_met(&answers_file);
-	first_meeting_place(&answers_file);
+int main() {
+    char answers_file[260];
+    bool realistic = true;
+    char genre_selected[30] = "";
+
+    create_answers_file(answers_file);
+    is_realistic(&realistic, answers_file);
+    genre(realistic, answers_file, genre_selected);
+
+    if (realistic) {
+        how_long_you_met(answers_file);
+        first_meeting_place(answers_file);
+    }
+    else {
+        if (strcmp(genre_selected, "Thriller") == 0) {
+            thriller_meeting_status(answers_file);
+        }
+        else if (strcmp(genre_selected, "Fantasy") == 0 || strcmp(genre_selected, "Science fiction") == 0) {
+            sci_fi_or_fantasy_meeting_place(answers_file);
+        }
+    }
+
+    return 0;
 }
+
+
--- vilan origin/questions.h
+++ vilan origin/questions.h
@@ -1,16 +1,41 @@
-#ifndef questions_H
-#define questions_H
+//#ifndef questions_H
+//#define questions_H
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//#include <stdbool.h>
+//
+//void is_realistic(bool* realistic, char* story_file_name);
+//void genre(bool realistic, char* story_file_name);
+//void how_long_you_met(char* story_file_name);
+//void first_meeting_place(char* story_file_name);
+//
+//
+//#endif
+//#pragma once
+
+#ifndef QUESTIONS_H
+#define QUESTIONS_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 
 void is_realistic(bool* realistic, char* story_file_name);
-void genre(bool realistic, char* story_file_name);
+
+void genre(bool realistic, char* story_file_name, char* genre_selected);
+
 void how_long_you_met(char* story_file_name);
 void first_meeting_place(char* story_file_name);
 
-
+void sci_fi_or_fantasy_meeting_place(char* story_file_name);
+void thriller_meeting_status(char* story_file_name);
+void hero_villain_same_side(char* story_file_name);
+void how_long_since_meeting(char* story_file_name);
+void thriller_meeting_place(char* story_file_name);
+void thriller_hero_duty(char* story_file_name);
+void was_one_crew_member(char* story_file_name);
 
 #endif
-#pragma once
+
