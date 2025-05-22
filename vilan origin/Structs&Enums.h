70ef837a7a0b2923cc8e830738db582fc383c2ee
lironshustin1996 <lironshustin1996@gmail.com>
2025-05-22 12:26:02 +0300

    I changed the structs in structs&enums

--- vilan origin/Structs&Enums.h
+++ vilan origin/Structs&Enums.h
@@ -6,52 +6,106 @@
 typedef int BOOL;
 
 typedef struct {
-	int how_long_known; //1) justMet 2) lessThanAYear 3) moreThanAyear
-	int where_met; //1) Family 2) studiedTogether 3) Work
+	char name[50];
+	char is_realistic[50]; //1) yes 2) no
+	char genre[50]; //1) Drama 2) Comedy
+	char how_long_known[50]; //1) justMet 2) lessThanAYear 3) moreThanAyear
+	char where_met[50]; //1) Family 2) studiedTogether 3) Work
+	char traits[100];
+	char villain_target[300];
+	char plot_line[1000];
 }drama_villain;
 
 typedef struct {
-	int how_long_known; //1) justMet 2) lessThanAYear 3) moreThanAyear
-	int where_met; //1) Family 2) studiedTogether 3) Work
+	char name[50];
+	char is_realistic[50]; //1) yes 2) no
+	char genre[50]; //1) Drama 2) Comedy
+	char how_long_known[50]; //1) justMet 2) lessThanAYear 3) moreThanAyear
+	char where_met[50]; //1) Family 2) studiedTogether 3) Work
+	char traits[100];
+	char villain_target[300];
+	char plot_line[1000];
 }comedy_villain;
 
 typedef struct {
-	int meeting_place; //1) enemy_captivity 2) Battlefield
-	int same_side; //1) yes 2) no
-	int how_long_sience_than; //1) justMet 2) lessThanAweek 3) moreThanAWeek 4) moreThanAYear
+	char name[50];
+	char is_realistic[50]; //1) yes 2) no
+	char genre[50]; //1) Drama 2) Comedy
+	char meeting_place[50]; //1) enemy_captivity 2) Battlefield
+	char same_side[50]; //1) yes 2) no
+	char how_long_sience_than[50]; //1) justMet 2) lessThanAweek 3) moreThanAWeek 4) moreThanAYear
+	char traits[100];
+	char villain_target[300];
+	char plot_line[1000];
 }fantasy_villain1;
 
 typedef struct {
-	int meeting_place; //3) canteen
-	int crew_member; //1) yes 2) no
+	char name[50];
+	char is_realistic[50]; //1) yes 2) no
+	char genre[50]; //1) Drama 2) Comedy
+	char meeting_place[50]; //3) canteen
+	char crew_member[50]; //1) yes 2) no
+	char traits[100];
+	char villain_target[300];
+	char plot_line[1000];
 }fantasy_villain2;
 
 typedef struct {
-	int meeting_place; //1) enemy_captivity 2) Battlefield
-	int same_side; //1) yes 2) no
-	int how_long_sience_than; //1) justMet 2) lessThanAweek 3) moreThanAWeek 4) moreThanAYear
+	char name[50];
+	char is_realistic[50]; //1) yes 2) no
+	char genre[50]; //1) Drama 2) Comedy
+	char meeting_place[50]; //1) enemy_captivity 2) Battlefield
+	char same_side[50]; //1) yes 2) no
+	char how_long_sience_than[50]; //1) justMet 2) lessThanAweek 3) moreThanAWeek 4) moreThanAYear
+	char traits[100];
+	char villain_target[300];
+	char plot_line[1000];
 }scifi_villain1;
 
 typedef struct {
-	int meeting_place; //3) canteen
-	int crew_member; //1) yes 2) no
+	char name[50];
+	char is_realistic[50]; //1) yes 2) no
+	char genre[50]; //1) Drama 2) Comedy
+	char meeting_place[50]; //3) canteen
+	char crew_member[50]; //1) yes 2) no
+	char traits[100];
+	char villain_target[300];
+	char plot_line[1000];
 }scifi_villain2;
 
 typedef struct {
-	int did_they_meet; //1) yes
-	int where; //1) canteen
-	int crew_member; //1) yes 2) no
+	char name[50];
+	char is_realistic[50]; //1) yes 2) no
+	char genre[50]; //1) Drama 2) Comedy
+	char did_they_meet[50]; //1) yes
+	char where[50]; //1) canteen
+	char crew_member[50]; //1) yes 2) no
+	char traits[100];
+	char villain_target[300];
+	char plot_line[1000];
 }thriller_villain1;
 
 typedef struct {
-	int did_they_meet; //1) yes
-	int where; //2) investigationOffice
-	int hero_duty; //1) investigator 2) murderer 3) victim 4) uninvolved
+	char name[50];
+	char is_realistic[50]; //1) yes 2) no
+	char genre[50]; //1) Drama 2) Comedy
+	char did_they_meet[50]; //1) yes
+	char where[50]; //2) investigationOffice
+	char hero_duty[50]; //1) investigator 2) murderer 3) victim 4) uninvolved
+	char traits[100];
+	char villain_target[300];
+	char plot_line[1000];
 }thriller_villain2;
 
 typedef struct {
-	int did_they_meet; //2) no
-	int hero_duty; //1) investigator 2) murderer 3) victim 4) uninvolved
+	char name[50];
+	char is_realistic[50]; //1) yes 2) no
+	char genre[50]; //1) Drama 2) Comedy
+	char did_they_meet[50]; //2) no
+	char hero_duty[50]; //1) investigator 2) murderer 3) victim 4) uninvolved
+	char traits[100];
+	char villain_target[300];
+	char plot_line[1000];
 }thriller_villain3;
 
 #endif 
